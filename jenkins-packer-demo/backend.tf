8ce4fa61c0e55208bf6a149c447efb4a018c502a
Sreedhar Chidambaram <sreedhar@capterra.com>
2018-10-16 12:55:55 -0400

    Fix bucket name

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-a3c731f"
+    bucket = "terraform-state-a2b621f-sreedhar"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
